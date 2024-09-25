a69aa7bf1f69abbd935a1744e979c1d4754dcf60
lukasz <l.bielecki13@gmail.com>
2024-09-25 23:09:54 +0200

    creating folders structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import './App.scss';
+import { SudokuBox } from './components/SudokuBox/SudokuBox';
+
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Sudoku Solver</h1>
+      <div>
+        <SudokuBox />
+        <button>TEST</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/SudokuBox/SudokuBox.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import styles from './SudokuBox.module.scss';
+
+export const SudokuBox: React.FC = () => {
+  return <div className={styles.wrapper}></div>;
+};
