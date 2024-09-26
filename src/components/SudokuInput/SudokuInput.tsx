2382ca8277a8b9a33f4d49dc1ec91ffcecb88224
lukasz <l.bielecki13@gmail.com>
2024-09-26 20:17:51 +0200

    adding basic input component

---
+++ src/components/SudokuInput/SudokuInput.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { SudokuInputProps } from './SudokuInput.types';
+
+import styles from './SudokuInput.module.scss';
+
+export const SudokuInput: React.FC<SudokuInputProps> = ({ value, onChange }) => {
+  return (
+    <input
+      className={styles.input}
+      type='text'
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      maxLength={1}
+    />
+  );
+};
---
+++ src/components/SudokuInput/SudokuInput.types.ts
@@ -0,0 +1,4 @@
+export interface SudokuInputProps {
+  value: string;
+  onChange: (value: string) => void;
+}
