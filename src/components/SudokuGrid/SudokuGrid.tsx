c156c216c563089570999d9b1e9fafeaa3cef0d1
lukasz <l.bielecki13@gmail.com>
2025-07-29 21:15:54 +0200

    creating initial grid

--- src/components/SudokuGrid/SudokuGrid.tsx
+++ src/components/SudokuGrid/SudokuGrid.tsx
@@ -1,5 +1,32 @@
+import { SudokuInput } from '@components/SudokuInput/SudokuInput';
+
 import styles from './SudokuGrid.module.scss';
 
+const initialState = [
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null],
+];
+
 export const SudokuGrid: React.FC = () => {
-  return <div className={styles.wrapper}></div>;
+  console.log('initialState', initialState);
+  return (
+    <div className={styles.wrapper}>
+      {initialState.map((row, rowIndex) => (
+        <ol key={rowIndex} className={styles.row}>
+          {row.map((cell, cellIndex) => (
+            <li key={cellIndex} className={styles.cell}>
+              <SudokuInput value={cell || `${rowIndex},${cellIndex}`} onChange={() => {}} />
+            </li>
+          ))}
+        </ol>
+      ))}
+    </div>
+  );
 };
