2a4b1d7e5ffdb7675903b7c1567705729506aa79
lukasz <l.bielecki13@gmail.com>
2025-07-30 21:19:53 +0200

    updating SudokuInput props

--- src/components/SudokuInput/SudokuInput.tsx
+++ src/components/SudokuInput/SudokuInput.tsx
@@ -4,14 +4,17 @@ import { SudokuInputProps } from './SudokuInput.types';
 
 import styles from './SudokuInput.module.scss';
 
-export const SudokuInput: React.FC<SudokuInputProps> = ({ value, onChange }) => {
+export const SudokuInput: React.FC<SudokuInputProps> = ({ id, value, onChange, ...props }) => {
   return (
     <input
+      id={id}
       className={styles.input}
       type="text"
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
+      value={value ?? ''}
       maxLength={1}
+      autoComplete="off"
+      onChange={onChange}
+      {...props}
     />
   );
 };
